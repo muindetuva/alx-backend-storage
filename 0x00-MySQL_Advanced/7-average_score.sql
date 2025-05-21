014382f86e65cc620f26a8fa380a5a7444842c92
Alfred Tuva <muindetuva@gmail.com>
2025-05-21 15:34:49 +0000

    Fix variable

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,19 +2,19 @@
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN in_user_id INT)
 BEGIN
   DECLARE avg_score FLOAT;
 
   -- Calculate average score from corrections for this user
   SELECT AVG(score) INTO avg_score
   FROM corrections
-  WHERE user_id = user_id;
+  WHERE user_id = in_user_id;
 
   -- Update user's average_score
   UPDATE users
   SET average_score = avg_score
-  WHERE id = user_id;
+  WHERE id = in_user_id;
 END$$
 
 DELIMITER ;
